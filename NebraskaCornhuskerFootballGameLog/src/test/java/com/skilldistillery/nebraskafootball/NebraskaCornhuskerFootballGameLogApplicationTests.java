4518ee7a4bde5afa838c9755f3b1c466945bc16b
jrpischel <jrpischel@gmail.com>
2023-10-06 18:57:01 -0400

    create spring boot REST project, implememnt dependicies

---
+++ NebraskaCornhuskerFootballGameLog/src/test/java/com/skilldistillery/nebraskafootball/NebraskaCornhuskerFootballGameLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.nebraskafootball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebraskaCornhuskerFootballGameLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
