4028cb7bdf96ebe1ca79aca82b22ab7fdb67533b
jrpischel <jrpischel@gmail.com>
2023-10-23 11:37:11 -0400

    location fix

--- NebraskaFootballGames/src/app/components/game/game.component.ts
+++ NebraskaFootballGames/src/app/components/game/game.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Game } from 'src/app/models/game';
+import { Location } from 'src/app/models/location';
 import { Season } from 'src/app/models/season';
 import { GameService } from 'src/app/services/game.service';
+import { LocationService } from 'src/app/services/location.service';
 import { SeasonService } from 'src/app/services/season.service';
 
 @Component({
@@ -13,6 +15,7 @@ import { SeasonService } from 'src/app/services/season.service';
 export class GameComponent {
   games: Game[] = [];
   seasons: Season[] = [];
+  locations: Location[] = [];
   selectedGame: Game | null = null;
   newGame: Game = new Game();
   editGame: Game | null = null;
@@ -22,10 +25,13 @@ export class GameComponent {
     private seasonService: SeasonService,
     private gameService: GameService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private locationService: LocationService
   ) {}
 
   ngOnInit(): void {
+    this.loadLocations();
+    this.loadSeasons();
     this.activatedRoute.paramMap.subscribe({
       next: (params) => {
         let seasonYearStr = params.get('seasonYear');
@@ -61,7 +67,32 @@ export class GameComponent {
     });
   }
 
+  loadLocations() {
+    this.locationService.index().subscribe({
+      next: (locations) => {
+        this.locations = locations;
+      },
+      error: (problem) => {
+        console.error('GameComponent.loadGames(): error loading Games:');
+        console.error(problem);
+      },
+    });
+  }
+
+  loadSeasons() {
+    this.seasonService.index().subscribe({
+      next: (seasons) => {
+        this.seasons = seasons;
+      },
+      error: (problem) => {
+        console.error('GameComponent.loadGames(): error loading Games:');
+        console.error(problem);
+      },
+    });
+  }
+
   addGame(seasonYear: number, game: Game) {
+    console.log(game);
     this.gameService.create(seasonYear, game).subscribe({
       next: (result) => {
         this.newGame = new Game();
--- NebraskaFootballGames/src/app/services/location.service.ts
+++ NebraskaFootballGames/src/app/services/location.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Location } from '../models/location';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
 
-  constructor() { }
+  private url = environment.baseUrl + 'api/games/locations';
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  index(): Observable<Location[]> {
+    return this.http.get<Location[]>(this.url) .pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('GameService.index(): error retrieving List of Games: ' + err)
+        );
+      })
+    );
+  }
 }
